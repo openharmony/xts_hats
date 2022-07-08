e6b621107c60f41e9ee4d000f0f67ae09c2b1af6
zhangxfy <zhangxiaofei37@huawei.com>
2022-07-08 14:51:13 +0800

    modified camera
    Signed-off-by: zhangxfy <zhangxiaofei37@huawei.com>
    Change-Id: I6fbd2afded72e59a2d8153c5f06a2833a4b3b37b

--- hdf/camera/v4l2/src/open_camera_test.cpp
+++ hdf/camera/v4l2/src/open_camera_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -209,11 +209,4 @@ HWTEST_F(OpenCameraTest, SUB_DriverSystem_CameraHdi_0020, TestSize.Level0)
                 std::cout << display_->rc << ", cameraId = " << cameraId << std::endl;
             }
         }
-}
-
-int main(int argc, char *argv[])
-{
-    // start running all tests
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
 }
--- hdf/camera/v4l2/src/stream_customer.cpp
+++ hdf/camera/v4l2/src/stream_customer.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,9 +15,10 @@
 
 #include "stream_customer.h"
 #include "video_key_info.h"
-
-StreamCustomer::StreamCustomer() {}
-StreamCustomer::~StreamCustomer() {}
+StreamCustomer::StreamCustomer()
+{}
+StreamCustomer::~StreamCustomer()
+{}
 
 void StreamCustomer::CamFrame(const std::function<void(void*, uint32_t)> callback)
 {

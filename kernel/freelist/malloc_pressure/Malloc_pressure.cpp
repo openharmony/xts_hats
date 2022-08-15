b500aaee56a33c948a4e59212ef8bfa34810e718
limeng151 <limeng151@huawei.com>
2022-08-15 10:59:17 +0800

    增加freelist测试用例
    Signed-off-by: limeng151 <limeng151@huawei.com>
    Change-Id: I334e67270c0aa207598cd6c52185582f4c3615a9

---
+++ kernel/freelist/malloc_pressure/Malloc_pressure.cpp
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <gtest/gtest.h>
+#include <cinttypes>
+#include <climits>
+#include <cstdio>
+#include <unistd.h>
+#include <string>
+
+#define SIZE_ALIGN (4 * sizeof(size_t))
+#define THRESHOLD (0x1c00 * SIZE_ALIGN)
+const int ITER_TIME = 20;
+
+using namespace testing::ext;
+using namespace std;
+
+class MallocPressure : public testing::Test {
+public:
+
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp();
+    void TearDown();
+private:
+};
+
+void MallocPressure::SetUp()
+{
+}
+void MallocPressure::TearDown()
+{
+}
+void MallocPressure::SetUpTestCase()
+{
+}
+void MallocPressure::TearDownTestCase()
+{
+}
+
+/*
+ * @tc.number CAM_FREE_FUN_0100
+ * @tc.name Apply part of the memory camfree reduction
+ * @tc.desc Test the basic features of CamFree
+*/
+HWTEST_F(MallocPressure, pressureTest0100, Function | MediumTest | Level1)
+{
+    struct timespec ts[2];
+    clock_gettime(CLOCK_REALTIME, &ts[0]);
+    for (int i = 0; i < ITER_TIME; ++i) {
+        for (size_t size = 0; size < THRESHOLD; size += SIZE_ALIGN + 1) {
+            void *ptr = malloc(size);
+            if (!ptr) {
+                printf("Malloc failed for size %u\n", size);
+                ASSERT_TRUE(true) << "malloc error = " << errno;
+            }
+            free(ptr);
+            ASSERT_TRUE(true) << "Abnormal operation of the system, errno" << errno;
+        }
+    }
+}
+
---
+++ kernel/freelist/safe_unlink/Malloc_safe_unlink.cpp
@@ -0,0 +1,160 @@
+/*
+ * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <gtest/gtest.h>
+#include <cinttypes>
+#include <climits>
+#include <cstdio>
+#include <unistd.h>
+#include <string>
+#include <sys/time.h>
+
+#define SIZE_ALIGN (4 * sizeof(size_t))
+#define THRESHOLD (0x1c00 * SIZE_ALIGN)
+#define ITER_TIME 20
+
+static void handler(int s)
+{
+}
+
+using namespace testing::ext;
+using namespace std;
+
+class MallocSafeUnlink : public testing::Test {
+public:
+
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp();
+    void TearDown();
+private:
+};
+
+void MallocSafeUnlink::SetUp()
+{
+}
+void MallocSafeUnlink::TearDown()
+{
+}
+void MallocSafeUnlink::SetUpTestCase()
+{
+}
+void MallocSafeUnlink::TearDownTestCase()
+{
+}
+
+volatile void *g_ptr;
+
+int set_devide_chunk(size_t size)
+{
+    if (!(g_ptr = malloc(size))) {
+        printf("Malloc failed: %s\n", strerror(errno));
+        return -1;
+    }
+    return 0;
+}
+
+static int child(void)
+{
+    uintptr_t *c;
+
+    /* Set first dividing chunk */
+    if (set_devide_chunk(sizeof(size_t))) {
+        return -1;
+    }
+
+    /*
+     * The init procedure makes the freelist unpredictable. To make sure trigger the safe-unlink
+     * check, Here we create as many chunks as possible to make sure there are enough chunks in
+     * bin[0] and malloc again. Basically this is heap spray.
+     */
+    for (int i = 0; i < 512; ++i) {
+        if (set_devide_chunk(sizeof(size_t))) {
+            return -1;
+        }
+        c = (uintptr_t *)malloc(sizeof(uintptr_t));
+        if (!c) {
+            printf("Malloc failed: %s\n", strerror(errno));
+            return -1;
+        }
+        free(c);
+        /* exchange the prev and next pointer */
+        uintptr_t temp = c[0];
+        c[0] = c[1];
+        c[1] = temp;
+    }
+
+    return 0;
+}
+
+static pid_t start_child(void)
+{
+    pid_t pid;
+    int ret;
+    pid = fork();
+    if (pid == 0) {
+        ret = child();
+        printf("child process normally out with %d\n", ret);
+        return ret;
+    }
+    return pid;
+}
+/*
+ * @tc.number CAM_FREE_FUN_0100
+ * @tc.name Apply part of the memory camfree reduction
+ * @tc.desc Test the basic features of CamFree
+*/
+HWTEST_F(MallocSafeUnlink, safeUnlinkTest0100, Function | MediumTest | Level1)
+{
+    sigset_t set;
+    int status;
+    pid_t pid;
+    int flag = 0;
+    struct timespec time1 = {5, 0};
+
+    sigemptyset(&set);
+    sigaddset(&set, SIGCHLD);
+    sigprocmask(SIG_BLOCK, &set, 0);
+    signal(SIGCHLD, handler);
+
+    pid = start_child();
+    if (pid == -1) {
+        printf("fork failed: %s\n", strerror(errno));
+    }
+    if (sigtimedwait(&set, 0, &time1) == -1) { /* Wait for 5 seconds */
+        if (errno == EAGAIN) {
+            flag = 1;
+        } else {
+            printf("sigtimedwait failed: %s\n", strerror(errno));
+        }
+        if (kill(pid, SIGKILL) == -1) {
+            printf("kill failed: %s\n", strerror(errno));
+        }
+    }
+
+    if (waitpid(pid, &status, 0) != pid) {
+        printf("waitpid failed: %s\n", strerror(errno));
+    }
+
+    if (flag) {
+        printf("Child process time out\n");
+    }
+
+    if (WIFSIGNALED(status)) {
+        ASSERT_TRUE(WTERMSIG(status) == SIGSEGV || WTERMSIG(status) == SIGILL) << "child process out with %s\n" <<
+        WTERMSIG(status);
+    } else {
+        ASSERT_TRUE(false) << "child process finished normally\n";
+    }
+}

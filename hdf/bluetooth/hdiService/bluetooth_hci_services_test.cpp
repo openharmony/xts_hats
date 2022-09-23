7d6236ec5ecea24d7f4836cebd7987bb659d02f2
quanli <quanli1@huawei.com>
2022-09-23 13:19:58 +0800

    quanli1@huawei.com
    Signed-off-by: quanli <quanli1@huawei.com>

--- hdf/bluetooth/hdiService/bluetooth_hci_services_test.cpp
+++ hdf/bluetooth/hdiService/bluetooth_hci_services_test.cpp
@@ -27,13 +27,25 @@
 
 using namespace OHOS::HDI::Bluetooth::Hci::V1_0;
 using namespace testing::ext;
-
+#define COMMAND_HCI_SHOULD_BE_UNKNOWN \
+    { 0xff, 0x3B, 0x08, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 }
+#define COMMAND_HCI_READ_LOCAL_VERSION_INFORMATION \
+    { 0x01, 0x10, 0x00 }
+#define COMMAND_HCI_READ_BUFFER_SIZE \
+    { 0x05, 0x10, 0x00 }
+#define COMMAND_HCI_RESET \
+    { 0x03, 0x0c, 0x00 }
+#define COMMAND_HCI_WRITE_LOCAL_NAME \
+    { 0x13, 0x0c, 0xf8 }
+#define ACL_BROADCAST_POINT_TO_POINT \
+    { 0x13, 0x0c, 0xf8 }
+#define ACL_PACKET_BOUNDARY_FIRST_AUTO_FLUSHABLE \
+    { 0x13, 0x0c, 0xf8 }
 namespace {
     sptr<IHciInterface>  g_iBtHci = nullptr;
     sptr<HciCallbackImpl> g_hciCallbacks = nullptr;
    
 }
-
 class HdfBluetoothHdiTest : public testing::Test {
 public:
     static void SetUpTestCase();
@@ -41,20 +53,16 @@ public:
     void SetUp();
     void TearDown();
 };
-
 void HdfBluetoothHdiTest::SetUpTestCase()
 {
     g_iBtHci = IHciInterface::Get();
 }
-
 void HdfBluetoothHdiTest::TearDownTestCase()
 {
 }
-
 void HdfBluetoothHdiTest::SetUp()
 {
 }
-
 void HdfBluetoothHdiTest::TearDown()
 {
 }
@@ -70,7 +78,6 @@ HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothInit_0100, TestSize.L
     if (g_hciCallbacks == nullptr) {
         return;
     }
-
     int32_t ret = g_iBtHci->Init(g_hciCallbacks);
     EXPECT_EQ(HDF_SUCCESS, ret);
 }
@@ -86,11 +93,36 @@ HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothGet_0200, TestSize.Le
 }
 
 /**
-  * @tc.name: HdiBluetoothSend_0300
-  * @tc.desc: the HCI ACL_DATA packets is sent successfully.
+  * @tc.name: HdiBluetoothSend_0100
+  * @tc.desc: the HCI ACL_DATA packets is sent success.
   * @tc.type: FUNC
   */
-HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothSend_0300, TestSize.Level1)
+HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothSend_0100, TestSize.Level2)
+{
+    if (g_iBtHci == nullptr) {
+        ASSERT_NE(nullptr, g_iBtHci);
+        return;
+    }
+    int n = 0;
+    std::vector<uint8_t> acl_vector;
+    acl_vector.push_back(static_cast<uint8_t>(0xff));
+    acl_vector.push_back(static_cast<uint8_t>((0x0f00) >> 8));
+    acl_vector.push_back(static_cast<uint8_t>(0xff));
+    acl_vector.push_back(static_cast<uint8_t>((0xff00) >> 8));
+    for (size_t i = 0; i < 10; i++) {
+        acl_vector.push_back(static_cast<uint8_t>(i + n));
+    }
+    BtType btType = BtType::ACL_DATA;
+    int32_t ret = g_iBtHci->SendHciPacket(btType, acl_vector);
+    EXPECT_EQ(HDF_SUCCESS, ret);
+}
+
+/**
+  * @tc.name: HdiBluetoothSend_0200
+  * @tc.desc: the HCI null ACL_DATA packets is sent.
+  * @tc.type: FUNC
+  */
+HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothSend_0200, TestSize.Level2)
 {
     if (g_iBtHci == nullptr) {
         ASSERT_NE(nullptr, g_iBtHci);
@@ -103,11 +135,112 @@ HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothSend_0300, TestSize.L
 }
 
 /**
-  * @tc.name: HdiBluetoothSend_0400
-  * @tc.desc: the HCI HCI_CMD packets is sent successfully.
+  * @tc.name: HdiBluetoothSend_0300
+  * @tc.desc: the reset HCI_CMD packets is sent successfully.
   * @tc.type: FUNC
   */
-HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothSend_0400, TestSize.Level1)
+HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothSend_0300, TestSize.Level2)
+{
+    if (g_iBtHci == nullptr) {
+        ASSERT_NE(nullptr, g_iBtHci);
+        return;
+    }
+    std::vector<uint8_t> cmd = COMMAND_HCI_RESET;
+    BtType btType = BtType::HCI_CMD;
+    int32_t ret = g_iBtHci->SendHciPacket(btType, cmd);
+    EXPECT_EQ(HDF_SUCCESS, ret);
+}
+
+/**
+  * @tc.name: HdiBluetoothSend_0400
+  * @tc.desc: the version HCI_CMD packets is sent successfully.
+  * @tc.type: FUNC
+  */
+HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothSend_0400, TestSize.Level2)
+{
+    if (g_iBtHci == nullptr) {
+        ASSERT_NE(nullptr, g_iBtHci);
+        return;
+    }
+    std::vector<uint8_t> cmd = COMMAND_HCI_READ_LOCAL_VERSION_INFORMATION;
+    BtType btType = BtType::HCI_CMD;
+    int32_t ret = g_iBtHci->SendHciPacket(btType, cmd);
+    EXPECT_EQ(HDF_SUCCESS, ret);
+}
+
+/**
+  * @tc.name: HdiBluetoothSend_0500
+  * @tc.desc: the unkonwn HCI_CMD packets is sent successfully.
+  * @tc.type: FUNC
+  */
+HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothSend_0500, TestSize.Level2)
+{
+    if (g_iBtHci == nullptr) {
+        ASSERT_NE(nullptr, g_iBtHci);
+        return;
+    }
+    std::vector<uint8_t> cmd = COMMAND_HCI_SHOULD_BE_UNKNOWN;
+    BtType btType = BtType::HCI_CMD;
+    int32_t ret = g_iBtHci->SendHciPacket(btType, cmd);
+    EXPECT_EQ(HDF_SUCCESS, ret);
+}
+
+/**
+  * @tc.name: HdiBluetoothSend_0600
+  * @tc.desc: the buffer HCI_CMD packets is sent successfully.
+  * @tc.type: FUNC
+  */
+HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothSend_0600, TestSize.Level2)
+{
+    if (g_iBtHci == nullptr) {
+        ASSERT_NE(nullptr, g_iBtHci);
+        return;
+    }
+    std::vector<uint8_t> cmd = COMMAND_HCI_READ_BUFFER_SIZE;
+    BtType btType = BtType::HCI_CMD;
+    int32_t ret = g_iBtHci->SendHciPacket(btType, cmd);
+    EXPECT_EQ(HDF_SUCCESS, ret);
+}
+
+/**
+  * @tc.name: HdiBluetoothSend_0700
+  * @tc.desc: the name HCI_CMD packets is sent successfully.
+  * @tc.type: FUNC
+  */
+HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothSend_0700, TestSize.Level2)
+{
+    if (g_iBtHci == nullptr) {
+        ASSERT_NE(nullptr, g_iBtHci);
+        return;
+    }
+     // Send an HCI packet
+    std::vector<uint8_t> writeName = COMMAND_HCI_WRITE_LOCAL_NAME;
+    // With a name
+    char newName[] = "John Jacob Jingleheimer Schmidt ___________________0";
+    size_t newNameLength = strlen(newName);
+    for (size_t i = 0; i < newNameLength; i++)
+        writeName.push_back(static_cast<uint8_t>(newName[i]));
+    // And the packet number
+    size_t i = newNameLength - 1;
+    size_t n = newNameLength;
+    for (int digits = n; digits > 0; digits = digits / 10, i--)
+        writeName[i] = static_cast<uint8_t>('0' + digits % 10);
+    // And padding
+    for (size_t i = 0; i < 248 - newNameLength; i++)
+        writeName.push_back(static_cast<uint8_t>(0));
+    std::vector<uint8_t> cmd = writeName;
+    BtType btType = BtType::HCI_CMD;
+    int32_t ret = g_iBtHci->SendHciPacket(btType, cmd);
+    EXPECT_EQ(HDF_SUCCESS, ret);
+}
+
+/**
+  * @tc.name: HdiBluetoothSend_0800
+  * uint16_t handle;
+  * size_t size;@tc.desc: the HCI null HCI_CMD packets is sent successfully.
+  * @tc.type: FUNC
+  */
+HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothSend_0800, TestSize.Level2)
 {
     if (g_iBtHci == nullptr) {
         ASSERT_NE(nullptr, g_iBtHci);
@@ -120,11 +253,36 @@ HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothSend_0400, TestSize.L
 }
 
 /**
-  * @tc.name: HdiBluetoothSend_0500
+  * @tc.name: HdiBluetoothSend_0900
   * @tc.desc: the HCI SCO_DATA packets is sent successfully.
   * @tc.type: FUNC
   */
-HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothSend_0500, TestSize.Level1)
+HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothSend_0900, TestSize.Level2)
+{
+    if (g_iBtHci == nullptr) {
+        ASSERT_NE(nullptr, g_iBtHci);
+        return;
+    }
+    int n = 0;
+    std::vector<uint8_t> sco_vector;
+    sco_vector.push_back(static_cast<uint8_t>(0xff));
+    sco_vector.push_back(static_cast<uint8_t>((0x0f00) >> 8));
+    sco_vector.push_back(static_cast<uint8_t>(0xff));
+    sco_vector.push_back(static_cast<uint8_t>((0xff00) >> 8));
+    for (size_t i = 0; i < 10; i++) {
+        sco_vector.push_back(static_cast<uint8_t>(i + n));
+    }
+    BtType btType = BtType::SCO_DATA;
+    int32_t ret = g_iBtHci->SendHciPacket(btType, sco_vector);
+    EXPECT_EQ(HDF_SUCCESS, ret);
+}
+
+/**
+  * @tc.name: HdiBluetoothSend_1000
+  * @tc.desc: the HCI SCO_DATA packets is sent successfully.
+  * @tc.type: FUNC
+  */
+HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothSend_1000, TestSize.Level2)
 {
     if (g_iBtHci == nullptr) {
         ASSERT_NE(nullptr, g_iBtHci);
@@ -137,11 +295,11 @@ HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothSend_0500, TestSize.L
 }
 
 /**
-  * @tc.name: HdiBluetoothSend_0600
+  * @tc.name: HdiBluetoothSend_1100
   * @tc.desc: the HCI HCI_EVENT packets is sent successfully.
   * @tc.type: FUNC
   */
-HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothSend_0600, TestSize.Level1)
+HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothSend_1100, TestSize.Level2)
 {
     if (g_iBtHci == nullptr) {
         ASSERT_NE(nullptr, g_iBtHci);
@@ -154,11 +312,11 @@ HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothSend_0600, TestSize.L
 }
 
 /**
-  * @tc.name: HdiBluetoothSend_0700
+  * @tc.name: HdiBluetoothSend_1200
   * @tc.desc: the HCI ISO_DATA packets is sent successfully.
   * @tc.type: FUNC
   */
-HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothSend_0700, TestSize.Level1)
+HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothSend_1200, TestSize.Level2)
 {
     if (g_iBtHci == nullptr) {
         ASSERT_NE(nullptr, g_iBtHci);
@@ -175,7 +333,7 @@ HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothSend_0700, TestSize.L
   * @tc.desc: Disable the HCI interface.
   * @tc.type: FUNC
   */
-HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothClose_0800, TestSize.Level1)
+HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothClose_0100, TestSize.Level3)
 {
     if (g_iBtHci == nullptr) {
         ASSERT_NE(nullptr, g_iBtHci);
@@ -184,4 +342,3 @@ HWTEST_F(HdfBluetoothHdiTest, SUB_DriverSystem_HdiBluetoothClose_0800, TestSize.
     int32_t ret = g_iBtHci->Close();
     EXPECT_EQ(HDF_SUCCESS, ret);
 }
-

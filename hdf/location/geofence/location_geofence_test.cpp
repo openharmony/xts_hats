ba3bd954d0a5b5fc3a359e86ebf7f34ee093f387
quanli <quanli1@huawei.com>
2022-11-16 15:31:23 +0800

    fixed ce40b28 from https://gitee.com/quanli125/xts_hats/pulls/390 quanli1@huawei.com
    Signed-off-by: quanli <quanli1@huawei.com>

---
+++ hdf/location/geofence/location_geofence_test.cpp
@@ -0,0 +1,179 @@
+/*
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <iostream>
+#include <osal_mem.h>
+#include "hdf_sbuf.h"
+#include <cmath>
+#include <cstdio>
+#include <unistd.h>
+#include <gtest/gtest.h>
+#include "idevmgr_hdi.h"
+#include <securec.h>
+#include "hdf_base.h"
+#include "hdf_log.h"
+#include "osal_time.h"
+#include "v1_0/igeofence_interface.h"
+#include "geofence_interface_impl.h"
+#include "geofence_callback_impl.h"
+
+using namespace OHOS::HDI::Location::Geofence::V1_0;
+using namespace std;
+using namespace testing::ext;
+
+namespace {
+    sptr<IGeofenceInterface> g_igeofenceHci = nullptr;
+    constexpr const char *AGNSS_SERVICE_NAME = "agnss_interface_service";
+    constexpr const char *GNSS_SERVICE_NAME = "gnss_interface_service";
+    constexpr const char *GEOFENCE_SERVICE_NAME = "geofence_interface_service";
+}
+
+class LocationGeofenceTest: public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp();
+    void TearDown();
+};
+
+int32_t GeofenceCallbackImpl::ReportGeofenceAvailability(bool isAvailable)
+{
+    return HDF_SUCCESS;
+}
+
+int32_t GeofenceCallbackImpl::ReportGeofenceEvent(int32_t fenceIndex, const LocationInfo& location, GeofenceEvent event, int64_t timestamp)
+{
+    return HDF_SUCCESS;
+}
+
+int32_t GeofenceCallbackImpl::ReportGeofenceOperateResult(int32_t fenceIndex, GeofenceOperateType type, GeofenceOperateResult result)
+{
+    return HDF_SUCCESS;
+}
+
+void LocationGeofenceTest::SetUpTestCase()
+{
+    auto devmgr = HDI::DeviceManager::V1_0::IDeviceManager::Get();
+    if (devmgr == nullptr) {
+        printf("fail to get devmgr.");
+        return;
+    }
+    if (devmgr->LoadDevice(GNSS_SERVICE_NAME) != 0) {
+        printf("Load gnss service failed!");
+        return;
+    }
+    if (devmgr->LoadDevice(AGNSS_SERVICE_NAME) != 0) {
+        printf("Load agnss service failed!");
+        return;
+    }
+    if (devmgr->LoadDevice(GEOFENCE_SERVICE_NAME) != 0) {
+        printf("Load geofence service failed!");
+        return;
+    }
+    g_igeofenceHci = IGeofenceInterface::Get();
+    printf("SetUpTestCase");
+}
+
+void LocationGeofenceTest::TearDownTestCase()
+{
+    auto devmgr = HDI::DeviceManager::V1_0::IDeviceManager::Get();
+    if (devmgr == nullptr) {
+        printf("fail to get devmgr.");
+        return;
+    }
+    if (devmgr->UnloadDevice(GNSS_SERVICE_NAME) != 0) {
+        printf("Load gnss service failed!");
+        return;
+    }
+    if (devmgr->UnloadDevice(AGNSS_SERVICE_NAME) != 0) {
+        printf("Load agnss service failed!");
+        return;
+    }
+    if (devmgr->UnloadDevice(GEOFENCE_SERVICE_NAME) != 0) {
+        printf("Load geofence service failed!");
+        return;
+    }
+    printf("TearDownTestCase");
+}
+
+void LocationGeofenceTest::SetUp()
+{
+    printf("SetUp");
+}
+
+void LocationGeofenceTest::TearDown()
+{
+    printf("TearDown");
+}
+
+
+/**
+  * @tc.name: SetGeofenceCallback0100
+  * @tc.desc: Set callback function for geofence.
+  * @tc.type: FUNC
+  */
+HWTEST_F(LocationGeofenceTest, SUB_DriverSystem_EnableGnss_0100, TestSize.Level1)
+{
+    if (g_igeofenceHci == nullptr) {
+        ASSERT_NE(nullptr, g_igeofenceHci);
+        return;
+    }
+    sptr<IGeofenceCallback> geo_callback = new (std::nothrow) GeofenceCallbackImpl();
+    if (geo_callback == nullptr) {
+        ASSERT_NE(nullptr, geo_callback);
+        return;
+    }
+    int32_t ret = g_igeofenceHci->SetGeofenceCallback(geo_callback);
+    EXPECT_EQ(HDF_SUCCESS, ret);
+}
+
+
+/**
+  * @tc.name: AddGnssGeofence0100
+  * @tc.desc: Add a geofence.
+  * @tc.type: FUNC
+  */
+HWTEST_F(LocationGeofenceTest, SUB_DriverSystem_AddGnssGeofence_0100, TestSize.Level1)
+{
+    if (g_igeofenceHci == nullptr) {
+        ASSERT_NE(nullptr, g_igeofenceHci);
+        return;
+    }
+    GeofenceInfo fence;
+    fence.fenceIndex = 3;
+    fence.latitude = 118.90;
+    fence.longitude = 15.25;
+    fence.radius = 12.26;
+    GeofenceEvent geoevent = GeofenceEvent::GEOFENCE_EVENT_UNCERTAIN ;
+    int32_t ret = g_igeofenceHci->AddGnssGeofence(fence,geoevent);
+    EXPECT_EQ(HDF_SUCCESS, ret);
+}
+
+
+/**
+  * @tc.name: DeleteGnssGeofence0100
+  * @tc.desc: Delete a geofence.
+  * @tc.type: FUNC
+  */
+HWTEST_F(LocationGeofenceTest, SUB_DriverSystem_DeleteGnssGeofence_0100, TestSize.Level1)
+{
+    if (g_igeofenceHci == nullptr) {
+        ASSERT_NE(nullptr, g_igeofenceHci);
+        return;
+    }
+    int fenceIndex = 5;
+    int32_t ret = g_igeofenceHci->DeleteGnssGeofence(fenceIndex);
+    EXPECT_EQ(HDF_SUCCESS, ret);
+}
---
+++ hdf/location/gnss/location_gnss_test.cpp
@@ -0,0 +1,374 @@
+/*
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include "idevmgr_hdi.h"
+#include <memory>
+#include <iostream>
+#include <osal_mem.h>
+#include "hdf_sbuf.h"
+#include <cmath>
+#include <cstdio>
+#include <unistd.h>
+#include <gtest/gtest.h>
+#include <securec.h>
+#include "hdf_base.h"
+#include "hdf_log.h"
+#include "osal_time.h"
+#include "v1_0/ignss_interface.h"
+#include "gnss_interface_impl.h"
+#include "gnss_callback_impl.h"
+
+
+using namespace OHOS::HDI::Location::Gnss::V1_0;
+using namespace std;
+using namespace testing::ext;
+
+namespace {
+    sptr<IGnssInterface> g_ignssHci = nullptr;
+    constexpr const char *AGNSS_SERVICE_NAME = "agnss_interface_service";
+    constexpr const char *GNSS_SERVICE_NAME = "gnss_interface_service";
+    constexpr const char *GEOFENCE_SERVICE_NAME = "geofence_interface_service";
+}
+
+class LocationGnssTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp();
+    void TearDown();
+};
+
+
+int32_t GnssCallbackImpl::ReportLocation(const LocationInfo& location)
+{
+    if (location.timeSinceBoot != 0) {
+        printf("Location success!!");
+        return HDF_SUCCESS;
+    }
+    else{
+        printf("Location fail!!");
+        return HDF_FAILURE;
+    }
+}
+
+
+int32_t GnssCallbackImpl::ReportGnssWorkingStatus(GnssWorkingStatus status)
+{
+    if (status == GnssWorkingStatus::GNSS_STATUS_NONE) {
+        printf("GNSS_STATUS_NONE");
+        return HDF_SUCCESS;
+    }
+    else if(status == GnssWorkingStatus::GNSS_STATUS_SESSION_BEGIN){
+        printf("GNSS_STATUS_SESSION_BEGIN");
+        return HDF_SUCCESS;
+    }
+    else if(status == GnssWorkingStatus::GNSS_STATUS_SESSION_END){
+        printf("GNSS_STATUS_SESSION_END");
+        return HDF_SUCCESS;
+    }
+    else if(status == GnssWorkingStatus::GNSS_STATUS_ENGINE_ON){
+        printf("GNSS_STATUS_ENGINE_ON");
+        return HDF_SUCCESS;
+    }
+    else if(status == GnssWorkingStatus::GNSS_STATUS_ENGINE_OFF){
+        printf("GNSS_STATUS_ENGINE_OFF");
+        return HDF_SUCCESS;
+    }
+    else{
+        printf("Gnss status fail");
+        return HDF_FAILURE;
+    }
+}
+
+int32_t GnssCallbackImpl::ReportNmea(int64_t timestamp, const std::string& nmea, int32_t length)
+{
+    if (length >= 0) {
+        printf("Report nmea success");
+        return HDF_SUCCESS;
+    }
+    else{
+        printf("Report nmea fail");
+        return HDF_FAILURE;
+    }
+}
+
+int32_t GnssCallbackImpl::ReportGnssCapabilities(GnssCapabilities capabilities)
+{
+    return HDF_SUCCESS;
+}
+
+int32_t GnssCallbackImpl::ReportSatelliteStatusInfo(const SatelliteStatusInfo& info)
+{
+    if (info.satellitesNumber <= 0) {
+        printf("SvStatusCallback, satellites_num <= 0!");
+        return HDF_ERR_INVALID_PARAM;
+    }
+    if (((info.carrierFrequencies).size()) > 0) {
+        printf("Get satellite info success!!");
+        return HDF_SUCCESS;
+    }
+    else{
+        printf("Get satellite info fail!!");
+        return HDF_FAILURE;
+    }
+}
+
+int32_t GnssCallbackImpl::RequestGnssReferenceInfo(GnssRefInfoType type)
+{
+    return HDF_SUCCESS;
+}
+
+int32_t GnssCallbackImpl::RequestPredictGnssData()
+{
+    return HDF_SUCCESS;
+}
+
+int32_t GnssCallbackImpl::ReportCachedLocation(const std::vector<LocationInfo>& gnssLocations)
+{
+    return HDF_SUCCESS;
+}
+
+
+
+void LocationGnssTest::SetUpTestCase()
+{
+    auto devmgr = HDI::DeviceManager::V1_0::IDeviceManager::Get();
+    if (devmgr == nullptr) {
+        printf("fail to get devmgr.");
+        return;
+    }
+    if (devmgr->LoadDevice(GNSS_SERVICE_NAME) != 0) {
+        printf("Load gnss service failed!");
+        return;
+    }
+    if (devmgr->LoadDevice(AGNSS_SERVICE_NAME) != 0) {
+        printf("Load agnss service failed!");
+        return;
+    }
+    if (devmgr->LoadDevice(GEOFENCE_SERVICE_NAME) != 0) {
+        printf("Load geofence service failed!");
+        return;
+    }
+    g_ignssHci = IGnssInterface::Get();
+    printf("SetUpTestCase");
+}
+
+void LocationGnssTest::TearDownTestCase()
+{
+    auto devmgr = HDI::DeviceManager::V1_0::IDeviceManager::Get();
+    if (devmgr == nullptr) {
+        printf("fail to get devmgr.");
+        return;
+    }
+    if (devmgr->UnloadDevice(GNSS_SERVICE_NAME) != 0) {
+        printf("Load gnss service failed!");
+        return;
+    }
+    if (devmgr->UnloadDevice(AGNSS_SERVICE_NAME) != 0) {
+        printf("Load agnss service failed!");
+        return;
+    }
+    if (devmgr->UnloadDevice(GEOFENCE_SERVICE_NAME) != 0) {
+        printf("Load geofence service failed!");
+        return;
+    }
+    printf("TearDownTestCase");
+}
+
+void LocationGnssTest::SetUp()
+{
+    printf("SetUp");
+}
+
+void LocationGnssTest::TearDown()
+{
+    printf("TearDown");
+}
+
+
+/**
+  * @tc.name: EnableGnss0100
+  * @tc.desc: Enable the GNSS module and transmit the callback of the upper layer to the GNSS module..
+  * @tc.type: FUNC
+  */
+HWTEST_F(LocationGnssTest, SUB_DriverSystem_EnableGnss_0100, TestSize.Level1)
+{
+    if (g_ignssHci == nullptr) {
+        ASSERT_NE(nullptr, g_ignssHci);
+        return;
+    }
+    sptr<IGnssCallback> gnss_callback = new (std::nothrow) GnssCallbackImpl();
+    if (gnss_callback == nullptr) {
+        ASSERT_NE(nullptr, gnss_callback);
+        return;
+    }
+    int32_t ret = g_ignssHci->EnableGnss(gnss_callback);
+    EXPECT_EQ(HDF_SUCCESS, ret);
+    GnssStartType starttype = GnssStartType::GNSS_START_TYPE_NORMAL;
+    int32_t ret1 = g_ignssHci->StartGnss(starttype);
+    EXPECT_EQ(HDF_SUCCESS, ret1);
+}
+
+
+/**
+  * @tc.name: SetGnssConfigPara0100
+  * @tc.desc: Setting gnss configuration parameters.
+  * @tc.type: FUNC
+  */
+HWTEST_F(LocationGnssTest, SUB_DriverSystem_SetGnssConfigPara_0100, TestSize.Level1)
+{
+    if (g_ignssHci == nullptr) {
+        ASSERT_NE(nullptr, g_ignssHci);
+        return;
+    }
+    GnssConfigPara para;
+    para.gnssBasic.minInterval = 10;
+    para.gnssBasic.accuracy = 50;
+    para.gnssBasic.firstFixTime = 300;
+    para.gnssBasic.isPeriodicPositioning = true;
+    para.gnssBasic.gnssMode = GnssWorkingMode::GNSS_WORKING_MODE_STANDALONE;
+    para.gnssCaching.interval = 20;
+    para.gnssCaching.fifoFullNotify = true;
+    int32_t ret = g_ignssHci->SetGnssConfigPara(para);
+    EXPECT_EQ(HDF_SUCCESS, ret);
+}
+
+
+/**
+  * @tc.name: SetGnssReferenceInfo0100
+  * @tc.desc: Inject reference information to the GNSS module.
+  * @tc.type: FUNC
+  */
+HWTEST_F(LocationGnssTest, SUB_DriverSystem_SetGnssReferenceInfo_0100, TestSize.Level1)
+{
+    if (g_ignssHci == nullptr) {
+        ASSERT_NE(nullptr, g_ignssHci);
+        return;
+    }
+    GnssRefInfo refInfo;
+    refInfo.type = GnssRefInfoType::GNSS_REF_INFO_TIME;
+    refInfo.time.timeMs = 50;
+    refInfo.time.timeReferenceMs = 100;
+    refInfo.time.uncertainty = 200;
+    refInfo.location.latitude = 39.56;
+    refInfo.location.longitude = 116.20;
+    refInfo.location.accuracy = 90;
+    refInfo.best_location.latitude = 39.58;
+    refInfo.best_location.longitude = 116.45;
+    refInfo.best_location.altitude = 110;
+    refInfo.best_location.accuracy = 60;
+    refInfo.best_location.speed = 60;
+    refInfo.best_location.direction = 60;
+    refInfo.best_location.timeStamp = 60;
+    refInfo.best_location.timeSinceBoot = 60;
+    int32_t ret = g_ignssHci->SetGnssReferenceInfo(refInfo);
+    EXPECT_EQ(HDF_SUCCESS, ret);
+}
+
+/**
+  * @tc.name: StopGnss0100
+  * @tc.desc: Stop the navigation function in normal mode.
+  * @tc.type: FUNC
+  */
+HWTEST_F(LocationGnssTest, SUB_DriverSystem_StopGnss_0100, TestSize.Level1)
+{
+    if (g_ignssHci == nullptr) {
+        ASSERT_NE(nullptr, g_ignssHci);
+        return;
+    }
+    GnssStartType stoptype = GnssStartType::GNSS_START_TYPE_NORMAL;
+    int32_t ret = g_ignssHci->StopGnss(stoptype);
+    EXPECT_EQ(HDF_SUCCESS, ret);  
+}
+
+/**
+  * @tc.name: DeleteAuxiliaryData0100
+  * @tc.desc: Delete the specified auxiliary data.
+  * @tc.type: FUNC
+  */
+HWTEST_F(LocationGnssTest, SUB_DriverSystem_DeleteAuxiliaryData_0100, TestSize.Level1)
+{
+    if (g_ignssHci == nullptr) {
+        ASSERT_NE(nullptr, g_ignssHci);
+        return;
+    }
+    GnssAuxiliaryData auxdata = GnssAuxiliaryData::GNSS_AUXILIARY_DATA_EPHEMERIS;
+    int32_t ret = g_ignssHci->DeleteAuxiliaryData(auxdata);
+    EXPECT_EQ(HDF_SUCCESS, ret);
+}
+
+
+/**
+  * @tc.name: SetPredictGnssData0100
+  * @tc.desc: Ingesting Predict Gnss Data.
+  * @tc.type: FUNC
+  */
+HWTEST_F(LocationGnssTest, SUB_DriverSystem_SetPredictGnssData_0100, TestSize.Level1)
+{
+    if (g_ignssHci == nullptr) {
+        ASSERT_NE(nullptr, g_ignssHci);
+        return;
+    }
+    const std::string str1 = "testing";
+    int32_t ret = g_ignssHci->SetPredictGnssData(str1);
+    EXPECT_EQ(HDF_SUCCESS, ret);
+}
+
+/**
+  * @tc.name: GetCachedGnssLocationsSize0100
+  * @tc.desc: Obtain the number of locations that can be cached by the GNSS module.
+  * @tc.type: FUNC
+  */
+HWTEST_F(LocationGnssTest, SUB_DriverSystem_GetCachedGnssLocationsSize_0100, TestSize.Level1)
+{
+    if (g_ignssHci == nullptr) {
+        ASSERT_NE(nullptr, g_ignssHci);
+        return;
+    }
+    int cach_size = 60;
+    int32_t ret = g_ignssHci->GetCachedGnssLocationsSize(cach_size);
+    EXPECT_EQ(HDF_SUCCESS, ret);
+}
+
+/**
+  * @tc.name: GetCachedGnssLocations0100
+  * @tc.desc: Request to obtain all the location information in the GNSS cache at a time and clear the cache buffer.
+  * @tc.type: FUNC
+  */
+HWTEST_F(LocationGnssTest, SUB_DriverSystem_GetCachedGnssLocations_0100, TestSize.Level1)
+{
+    if (g_ignssHci == nullptr) {
+        ASSERT_NE(nullptr, g_ignssHci);
+        return;
+    }
+    int32_t ret = g_ignssHci->GetCachedGnssLocations();
+    EXPECT_EQ(HDF_SUCCESS, ret);
+}
+
+/**
+  * @tc.name: DisableGnss0100
+  * @tc.desc: Disable the GNSS module.
+  * @tc.type: FUNC
+  */
+HWTEST_F(LocationGnssTest, SUB_DriverSystem_DisableGnss_0100, TestSize.Level1)
+{
+    if (g_ignssHci == nullptr) {
+        ASSERT_NE(nullptr, g_ignssHci);
+        return;
+    }
+    int32_t ret = g_ignssHci->DisableGnss();
+    EXPECT_EQ(HDF_SUCCESS, ret);
+}

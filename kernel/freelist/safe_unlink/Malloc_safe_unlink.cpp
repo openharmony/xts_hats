0b70d92d0746d4a9a490d801aa9f9db0aab5ded7
limeng151 <limeng151@huawei.com>
2023-04-23 16:31:19 +0800

    fix freelist testcase
    Signed-off-by: limeng151 <limeng151@huawei.com>
    Change-Id: I1cba12b866d49ecc1161a8a0feedfbd600676e68

--- kernel/freelist/safe_unlink/Malloc_safe_unlink.cpp
+++ kernel/freelist/safe_unlink/Malloc_safe_unlink.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021-2022 Huawei Device Co., Ltd.
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,24 +12,24 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <gtest/gtest.h>
-#include <cinttypes>
-#include <climits>
-#include <cstdio>
+
+#include <cstdlib>
 #include <unistd.h>
-#include <string>
-#include <sys/time.h>
+#include <sys/wait.h>
+#include <csignal>
+#include <cerrno>
+#include <cstring>
+#include <gtest/gtest.h>
 
-#define SIZE_ALIGN (4 * sizeof(size_t))
-#define THRESHOLD (0x1c00 * SIZE_ALIGN)
-#define ITER_TIME 20
-
-static void handler(int s)
-{
-}
+#define UNIT            16
+#define OFF_OFFSET      2
+#define FIRST_OFFSET    -4
+#define FIRST_OFF_OFFSET 8
+#define MALLOC_SIZE_S   40
+#define MALLOC_SIZE_L   500
+#define TEST_NUM        512
 
 using namespace testing::ext;
-using namespace std;
 
 class MallocSafeUnlink : public testing::Test {
 public:
@@ -54,55 +54,76 @@ void MallocSafeUnlink::TearDownTestCase()
 {
 }
 
-volatile void *g_ptr;
+struct meta_in {
+    struct meta_in *prev, *next;
+    uintptr_t *mem;
+};
+
+struct group_in {
+    struct meta_in *meta;
+};
+
+static struct group_in *get_group(volatile const uint8_t *p)
+{
+    int offset = *(const uint16_t *)((uintptr_t)p - OFF_OFFSET);
+
+    if (p[FIRST_OFFSET]) {
+        offset = *(uintptr_t *)((uintptr_t)p - FIRST_OFF_OFFSET);
+    }
+
+    struct group_in *base = (struct group_in *)((uintptr_t)p - UNIT*offset - UNIT);
+    return base;
+}
+
+static void handler(int s)
+{
+}
+
+volatile void *tmp;
 
 int set_devide_chunk(size_t size)
 {
-    if (!(g_ptr = malloc(size))) {
+    if (!(tmp = malloc(size))) {
         printf("Malloc failed: %s\n", strerror(errno));
         return -1;
     }
     return 0;
 }
+extern int g_test_d;
 
 static int child(void)
 {
-    uintptr_t *c;
-    uintptr_t *d;
+    volatile uint8_t *c1;
+    volatile uint8_t *c2;
+    uint8_t *c1_tmp;
+    uint8_t *c2_tmp;
 
-    /* Set first dividing chunk */
-    if (set_devide_chunk(sizeof(uintptr_t))) {
-        return -1;
-    }
+    struct group_in *g1 = nullptr;
+    struct group_in *g2 = nullptr;
 
-    /*
-     * The init procedure makes the freelist unpredictable. To make sure trigger the safe-unlink
-     * check, Here we create as many chunks as possible to make sure there are enough chunks in
-     * bin[0] and malloc again. Basically this is heap spray.
-     */
-    for (int i = 0; i < 512; ++i) {
-        c = (uintptr_t *)malloc(sizeof(uintptr_t));
-        if (!c) {
+    for (int i = 0; i < TEST_NUM; ++i) {
+        c1_tmp = (uint8_t *)malloc(MALLOC_SIZE_S + 0x10 * i);
+        c2_tmp = (uint8_t *)malloc(MALLOC_SIZE_L + 0x10 * i);
+
+        c1 = (uint8_t *)malloc(MALLOC_SIZE_S + 0x10 * i);
+        if ((!c1) || ((uintptr_t)c1 & 15))  {
             printf("Malloc failed: %s\n", strerror(errno));
             return -1;
         }
-        if (set_devide_chunk(sizeof(size_t))) {
+        g1 = get_group(c1);
+
+        c2 = (uint8_t *)malloc(MALLOC_SIZE_L + 0x10 * i);
+        if ((!c2) || ((uintptr_t)c2 & 15))  {
+            printf("Malloc failed: %s\n", strerror(errno));
             return -1;
         }
-        d=(uintptr_t *)malloc(sizeof(uintptr_t));
-        if (!d) {
-            printf("Malloc failed : %s\n", strerror(errno));
-            return -1;
-        }
-        if (set_devide_chunk(sizeof(size_t))) {
-            return -1;
-        }
-        free(d);
-        free(c);
-        /* exchange the prev and next pointer */
-        uintptr_t temp = c[0];
-        c[0] = c[1];
-        c[1] = temp;
+
+        g2 = get_group(c2);
+        g2->meta = g1->meta;
+
+        free((void *)c1);
+        free((void *)c2);
+
     }
 
     return 0;
@@ -120,13 +141,13 @@ static pid_t start_child(void)
     }
     return pid;
 }
+
 /*
- * @tc.number CAM_FREE_FUN_0100
+ * @tc.number SAFE_UNLINK_TEST_0100
  * @tc.name Apply part of the memory camfree reduction
- * @tc.desc Test the basic features of CamFree
+ * @tc.desc Test the safeunlink
 */
-HWTEST_F(MallocSafeUnlink, safeUnlinkTest0100, Function | MediumTest | Level1)
-{
+HWTEST_F(MallocSafeUnlink, safeunlinkTest0100, Function | MediumTest | Level1) {
     sigset_t set;
     int status;
     pid_t pid;
@@ -140,13 +161,14 @@ HWTEST_F(MallocSafeUnlink, safeUnlinkTest0100, Function | MediumTest | Level1)
 
     pid = start_child();
     if (pid == -1) {
-        printf("fork failed: %s\n", strerror(errno));
+        printf("fork failed: %s\n",strerror(errno));
     }
-    if (sigtimedwait(&set, 0, &time1) == -1) { /* Wait for 5 seconds */
+    if (sigtimedwait(&set, 0, &time1)  == -1) { /* Wait for 5 seconds */
         if (errno == EAGAIN) {
             flag = 1;
-        } else {
-            printf("sigtimedwait failed: %s\n", strerror(errno));
+        }
+        else {
+            printf("sigtimedwait failed: %s\n",strerror(errno));
         }
         if (kill(pid, SIGKILL) == -1) {
             printf("kill failed: %s\n", strerror(errno));

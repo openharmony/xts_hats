6734b3dc1e1fd151a05ffe6d7aa0ba64f5ea95a5
lujianhui <lujianhui10@huawei.com>
2023-08-08 15:18:06 +0800

    fix: Update dependency on display module
    Signed-off-by: lujianhui <lujianhui10@huawei.com>

--- hdf/camera/cameraHdi/v4l2/include/test_display.h
+++ hdf/camera/cameraHdi/v4l2/include/test_display.h
@@ -29,7 +29,7 @@
 #include "idevice_manager.h"
 #include "camera_metadata_info.h"
 #include "metadata_utils.h"
-#include <display_type.h>
+#include <v1_0/display_composer_type.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
@@ -81,6 +81,7 @@ constexpr int DEFAULT_STREAM_ID = 1001;
 constexpr int INVALID_VALUE_TEST = 2147483647;
 
 using namespace OHOS::HDI::Camera::V1_0;
+using namespace OHOS::HDI::Display::Composer::V1_0;
 using namespace OHOS::Camera;
 class TestDisplay {
 public:

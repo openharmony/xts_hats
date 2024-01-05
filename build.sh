6a690936c7b5881fdfca5c463acd94b564700fd5
openharmony_ci <120357966@qq.com>
2024-01-05 01:34:24 +0000

    回退 'Pull Request !940 : 新添加CACHE_ARG参数用来选择xcache作为cache类型'

--- build.sh
+++ build.sh
@@ -77,8 +77,6 @@ parse_cmdline()
                           ;;
         upload_api_info)  UPLOAD_API_INFO=$(echo $PARAM |tr [a-z] [A-Z])
                          ;;
-        cache_type)       CACHE_TYPE="$PARAM"
-                          ;;
         *)   usage
              break;;
         esac
@@ -108,11 +106,7 @@ do_make()
                         MUSL_ARGS="--gn-args use_musl=false --gn-args use_custom_libcxx=true --gn-args use_custom_clang=true"			
 		    fi
         fi
-        CACHE_ARG=""
-        if [ "$CACHE_TYPE" = "xcache" ]; then
-            CACHE_ARG="--ccache false --xcache true"
-        fi
-       ./build.sh --product-name $PRODUCT_NAME --gn-args build_xts=true --build-target $BUILD_TARGET --build-target "deploy_testtools" --gn-args is_standard_system=true $MUSL_ARGS --target-cpu $TARGET_ARCH --get-warning-list=false --stat-ccache=true --compute-overlap-rate=false --deps-guard=false --generate-ninja-trace=false $CACHE_ARG --gn-args skip_generate_module_list_file=true
+       ./build.sh --product-name $PRODUCT_NAME --gn-args build_xts=true --build-target $BUILD_TARGET --build-target "deploy_testtools" --gn-args is_standard_system=true $MUSL_ARGS --target-cpu $TARGET_ARCH --get-warning-list=false --stat-ccache=true --compute-overlap-rate=false --deps-guard=false --generate-ninja-trace=false --gn-args skip_generate_module_list_file=true
     else
        if [ "$BUILD_TARGET" = "hats hats_ivi hats_intellitv hats_wearable" ]; then
          ./build.sh --product-name $PRODUCT_NAME --gn-args build_xts=true --build-target "hats" --build-target "hats_ivi" --build-target "hats_intellitv" --build-target "hats_wearable" --build-target "deploy_testtools"

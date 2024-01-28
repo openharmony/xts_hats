7bde50b5634c50c9abc80e9aa25e677736048f3e
wangchao582 <wangchao582@huawei.com>
2024-01-28 11:19:20 +0800

    Modify display testDisplayBufferFreeMem001 and Copyright
    Signed-off-by: wangchao582 <wangchao582@huawei.com>

--- hdf/display/buffer/moduletest_additional/display_buffer_mt.cpp
+++ hdf/display/buffer/moduletest_additional/display_buffer_mt.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2023-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -3659,7 +3659,11 @@ HWTEST_F(DisplayBufferMtAdditional, testDisplayBufferFreeMem001, Function | Medi
     g_dispbuf->AllocMem(info, bHandle);
     g_dispbuf->FreeMem(*bHandle);
     void *buffer = g_dispbuf->Mmap(*bHandle);
-    EXPECT_TRUE((buffer == reinterpret_cast<void *>(0xffffffff)) || (buffer == nullptr));
+#ifdef DISPLAY_COMMUNITY
+    EXPECT_TRUE(buffer == reinterpret_cast<void *>(0xffffffff));
+#else
+    EXPECT_TRUE(buffer == nullptr);
+#endif // DISPLAY_COMMUNITY
 }
 
 } // namespace TEST

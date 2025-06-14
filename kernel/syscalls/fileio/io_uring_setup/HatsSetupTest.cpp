ba720443823d4beb7732c54b3cb051a041bbd0f6
songxingxing <songxingxing12@h-partners.com>
2025-06-14 11:32:57 +0800

    [xts-hats]test enter codecheck
    Signed-off-by: songxingxing <songxingxing12@h-partners.com>

--- kernel/syscalls/fileio/io_uring_setup/HatsSetupTest.cpp
+++ kernel/syscalls/fileio/io_uring_setup/HatsSetupTest.cpp
@@ -26,18 +26,16 @@
 #include  "securec.h"
 #include  <linux/io_uring.h>
 #include  <sys/mman.h>
-#include  <sys/syscall.h>  
+#include  <sys/syscall.h>
 
 static int io_uring_setup(unsigned entries,struct io_uring_params *p){
         return syscall(__NR_io_uring_setup,entries,p);
 }
 
-
 using namespace testing::ext;
 
 static const char  *TEST_VALID_PATH = "/data/local/tmp/attr_test_dir";
 static const char  *TEST_VALID_FILE = "/data/local/tmp/attr_test_dir/attr_test.txt";
-
 
 class HatsSetupTest:public testing::Test{
 public:
@@ -46,30 +44,25 @@ static void TearDownTestCase();
 void SetUp();
 void TearDown();
 private:
-
 };
 
 void HatsSetupTest::SetUp()
 {
-int fd = -1;
-if(access(TEST_VALID_PATH,F_OK) == 0)
-{
-    (void)remove(TEST_VALID_PATH);
+    int fd = -1;
+    if (access(TEST_VALID_PATH,F_OK) == 0) {
+        (void)remove(TEST_VALID_PATH);
+    }
+    (void)mkdir(TEST_VALID_PATH, S_IWUSR | S_IRUSR | S_IXUSR);
+    fd = open(TEST_VALID_FILE, O_WRONLY | O_CREAT,S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    close(fd);
 }
-(void)mkdir(TEST_VALID_PATH,S_IWUSR | S_IRUSR | S_IXUSR);
-fd = open(TEST_VALID_FILE,O_WRONLY | O_CREAT,S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-close(fd);
-
-}
-
 void HatsSetupTest::TearDown()
 {
-(void)remove(   TEST_VALID_FILE);
-if(access(TEST_VALID_PATH,F_OK) == 0){
-    (void)remove(TEST_VALID_PATH);
+    (void)remove(TEST_READ_FILE);
+    if (access(TEST_VALID_PATH,F_OK) == 0) {
+        (void)remove(TEST_VALID_PATH);
+    }
 }
-}
-
 void HatsSetupTest::SetUpTestCase()
 {
     static io_uring_params p = {0};
@@ -78,12 +71,9 @@ void HatsSetupTest::SetUpTestCase()
         GTEST_SKIP() << "Not support wearable, skip testCase";
     }
 }
-
 void HatsSetupTest::TearDownTestCase()
 {
-    
 }
-
 
 /**
 * @tc.number : SUB_BASIC_FM_USE_IOURING_SETUP_0100
@@ -93,30 +83,25 @@ void HatsSetupTest::TearDownTestCase()
 * @tc.type   : Function
 * @tc.level  : Level 1
 */
-
-HWTEST_F(HatsSetupTest,SetupApiTestSuccess_0001,Function | MediumTest | Level1)
+HWTEST_F(HatsSetupTest, SetupApiTestSuccess_0001, Function | MediumTest | Level1)
 {
-
     static io_uring_params p = {0};
 
-    int uring_fd01 = io_uring_setup(1,&p);
-    EXPECT_TRUE(uring_fd01 > 0);
+    int uringFd01 = io_uring_setup(1, &p);
+    EXPECT_TRUE(uringFd01 > 0);
 
-    int uring_fd02 = io_uring_setup(2,&p);
-    EXPECT_TRUE(uring_fd02 > 0);
+    int uringFd02 = io_uring_setup(2, &p);
+    EXPECT_TRUE(uringFd02 > 0);
 
-    int uring_fd03 = io_uring_setup(8,&p);
-    EXPECT_TRUE(uring_fd03 > 0);
+    int uringFd03 = io_uring_setup(8, &p);
+    EXPECT_TRUE(uringFd03 > 0);
 
-    int uring_fd04 = io_uring_setup(32767,&p);
-    EXPECT_TRUE(uring_fd04 > 0);
+    int uringFd04 = io_uring_setup(32767, &p);
+    EXPECT_TRUE(uringFd04 > 0);
 
-    int uring_fd05 = io_uring_setup(32768,&p);
-    EXPECT_TRUE(uring_fd05 > 0);
-
+    int uringFd05 = io_uring_setup(32768, &p);
+    EXPECT_TRUE(uringFd05 > 0);
 }
-
-
 
 /**
 * @tc.number : SUB_BASIC_FM_USE_IOURING_SETUP_0200
@@ -126,15 +111,11 @@ HWTEST_F(HatsSetupTest,SetupApiTestSuccess_0001,Function | MediumTest | Level1)
 * @tc.type   : Function
 * @tc.level  : Level 1
 */
-
-HWTEST_F(HatsSetupTest,SetupApiTest_params_null_0002,Function | MediumTest | Level1)
+HWTEST_F(HatsSetupTest, SetupApiTest_params_null_0002, Function | MediumTest | Level1)
 {
-
-    io_uring_setup(1,NULL);
+    io_uring_setup(1, nullptr);
     EXPECT_EQ(errno, 14);
-
 }
-
 
 /**
 * @tc.number : SUB_BASIC_FM_USE_IOURING_SETUP_0300
@@ -144,24 +125,21 @@ HWTEST_F(HatsSetupTest,SetupApiTest_params_null_0002,Function | MediumTest | Lev
 * @tc.type   : Function
 * @tc.level  : Level 1
 */
-
-HWTEST_F(HatsSetupTest,SetupApiTestFail_0003,Function | MediumTest | Level1)
+HWTEST_F(HatsSetupTest, SetupApiTestFail_0003, Function | MediumTest | Level1)
 {
+    static io_uring_params p = {0};
 
-     static io_uring_params p = {0};
-
-    int uring_fd01 = io_uring_setup(-1, &p);
-    EXPECT_EQ(uring_fd01, -1);
+    int uringFd01 = io_uring_setup(-1, &p);
+    EXPECT_EQ(uringFd01, -1);
     EXPECT_EQ(errno, 22);
 
-    int uring_fd02 = io_uring_setup(0, &p);
-    EXPECT_EQ(uring_fd02, -1);
+    int uringFd02 = io_uring_setup(0, &p);
+    EXPECT_EQ(uringFd02, -1);
     EXPECT_EQ(errno, 22);
 
-    int uring_fd03 = io_uring_setup(32769, &p);
-    EXPECT_EQ(uring_fd03, -1);
+    int uringFd03 = io_uring_setup(32769, &p);
+    EXPECT_EQ(uringFd03, -1);
     EXPECT_EQ(errno, 22);
-
 }
 
 /**
@@ -172,14 +150,10 @@ HWTEST_F(HatsSetupTest,SetupApiTestFail_0003,Function | MediumTest | Level1)
 * @tc.type   : Function
 * @tc.level  : Level 1
 */
-
 HWTEST_F(HatsSetupTest,SetupApiTest_params_int_0004,Function | MediumTest | Level1)
 {
-
      static io_uring_params params;
-     memset(&params, 0, sizeof(int));
-
-    io_uring_setup(8, &params);
-    EXPECT_EQ(errno, 2);
-
+     memset_s(&params, sizeof(int), 0, sizeof(int));
+     io_uring_setup(8, &params);
+     EXPECT_EQ(errno, 2);
 }
